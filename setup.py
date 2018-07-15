218ded3f7d62aa61dafea8d0fa3a337c76b21571
Suyash458 <suyash.behera458@gmail.com>
2018-07-15 16:05:54 +0530

    Added pronoun to parts of speech list Fix: definitions with index > 9 being skipped Fix: definitions with empty etymologies being skipped

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='wiktionaryparser',
-      version='0.1',
-      description='python library to interface with wiktionary online via json',
-      url='https://github.com/Suyash458/WiktionaryParser',
-      packages=['wiktionaryparser'],
-      zip_safe=False)
