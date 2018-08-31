ea6f046ba6a182bc70ea3c42fa0e81c750f602d9
Nicolas Patry <nicolas@kwyk.fr>
2018-08-31 11:25:03 +0200

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='WiktionaryParser',
+    version='0.1',
+    description='Parser for wiktionary',
+    author='John doe',
+    author_email='john@doe.com',
+    url='https://www.python.org/sigs/distutils-sig/',
+    packages=['wiktionaryparser', 'wiktionaryparser.utils']
+)
