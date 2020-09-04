d462467b83d64390c31309c1b4191668724a1801
sehwol <sehwol@users.noreply.github.com>
2020-09-04 23:13:18 +0700

    test: restructure tests for better clarity
    Have each test only test one word at a time and make sure erronous
    results are printed out as an indented JSON.

--- tests/test.py
+++ tests/test.py
@@ -1,27 +1,88 @@
-import unittest, json
+from parameterized import parameterized
+import unittest
+import json
 from .context import WiktionaryParser
 from deepdiff import DeepDiff
+from typing import Dict
 
 parser = WiktionaryParser()
 
+
 class TestParser(unittest.TestCase):
-    def test_multiple_languages(self):
-        sample_output = {}
+    def __init__(self, *args, **kwargs):
+        self.expected_results = {}
+
         with open('tests/testOutput.json', 'r') as f:
-            sample_output = json.load(f)
-        words_to_test = {
-            'English': {'grapple': 50080840, 'test': 50342756, 'patronise': 49023308, 'abiologically': 43781266, 'alexin': 50152026, 'song': 50235564, 'house': 50356446},
-            'Latin': {'video': 50291344},
-            'Norwegian Bokmål': {'seg': 50359832, 'aldersblandet': 38616917, 'by': 50399022, 'for': 50363295, 'admiral': 50357597, 'heis': 49469949, 'konkurs': 48269433, 'pantergaupe': 46717478, 'maldivisk': 49859434},
-            'Swedish': {'house': 50356446},
-            'Ancient Greek': {'ἀγγελία': 47719496}
-        }
-        for lang, words in words_to_test.items():
-            parser.set_default_language(lang)
-            for word, old_id in words.items():
-                parsed_word = parser.fetch(word, old_id=old_id)
-                print("Testing \"{}\" in {}".format(word, lang))
-                self.assertEqual(DeepDiff(parsed_word, sample_output[lang][word], ignore_order=True), {})
+            self.expected_results = json.load(f)
+
+        super(TestParser, self).__init__(*args, **kwargs)
+
+    @parameterized.expand([
+        ('grapple', 50080840),
+        ('test', 50342756),
+        ('patronise', 49023308),
+        ('abiologically', 43781266),
+        ('alexin', 50152026),
+        ('song', 50235564),
+        ('house', 50356446),
+    ])
+    def test_words_from_english(self, word: str, old_id: int):
+        self.__test_word(word, old_id, 'English')
+
+    @parameterized.expand([
+        ('video', 50291344),
+    ])
+    def test_words_from_latin(self, word: str, old_id: int):
+        self.__test_word(word, old_id, 'Latin')
+
+    @parameterized.expand([
+        ('seg', 50359832),
+        ('aldersblandet', 38616917),
+        ('by', 50399022),
+        ('for', 50363295),
+        ('admiral', 50357597),
+        ('heis', 49469949),
+        ('konkurs', 48269433),
+        ('pantergaupe', 46717478),
+        ('maldivisk', 49859434),
+    ])
+    def test_words_from_norwegian_bokmal(self, word: str, old_id: int):
+        self.__test_word(word, old_id, 'Norwegian Bokmål')
+
+    @parameterized.expand([
+        ('house', 50356446)
+    ])
+    def test_words_from_swedish(self, word: str, old_id: int):
+        self.__test_word(word, old_id, 'Swedish')
+
+    @parameterized.expand([
+        ('ἀγγελία', 47719496)
+    ])
+    def test_words_from_ancient_greek(self, word: str, old_id: int):
+        self.__test_word(word, old_id, 'Ancient Greek')
+
+    def __test_words(self, words_and_ids: Dict[str, int], lang: str):
+        for word, old_id in words_and_ids.items():
+            self.__test_word(word, old_id, lang)
+
+    def __test_word(self, word: str, old_id: int, lang: str):
+        parser.set_default_language(lang)
+        fetched_word = parser.fetch(word, old_id=old_id)
+
+        print("Testing \"{}\" in \"{}\"".format(word, lang))
+        expected_result = self.expected_results[lang][word]
+
+        diff = DeepDiff(fetched_word,
+                        expected_result,
+                        ignore_order=True)
+
+        if diff != {}:
+            print("Found mismatch in \"{}\" in \"{}\"".format(word, lang))
+            print(json.dumps(json.loads(diff.json), indent=4))
+
+        self.assertEqual(diff, {})
+
 
 if __name__ == '__main__':
     unittest.main()
+ 	
