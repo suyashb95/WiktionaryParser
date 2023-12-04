d6ce77bac55c6361f7b9fb21f7f61e06cc2449f4
ZOUHEIRBN <zouheir.b97@gmail.com>
2023-12-04 21:28:57 +0100

    Added graph export to pipeline

--- main.py
+++ main.py
@@ -4,21 +4,24 @@ from scripts.dataset_uploading import main as upload_data
 from scripts.datasets_to_tokens import convert_to_tokens, get_global_token_counts
 from scripts.get_word_info import main as collect_info
 from scripts.deorphanize import main as deorphanize
+from scripts.visualize_interactive_graph import export_graph_to_html
 
-EXPERIMENTAL = True
-reset_db()
-limit = 5 if EXPERIMENTAL else -1
-datasets = upload_data('D:\Datasets', limit=limit)
-tokenized_texts = convert_to_tokens(datasets)
-global_tokens = get_global_token_counts(tokenized_texts)
+# EXPERIMENTAL = True
+# reset_db()
+# limit = 5 if EXPERIMENTAL else -1
+# datasets = upload_data('D:\Datasets', limit=limit)
+# tokenized_texts = convert_to_tokens(datasets)
+# global_tokens = get_global_token_counts(tokenized_texts)
 
-if EXPERIMENTAL:
-    global_tokens = global_tokens[:10]
+# if EXPERIMENTAL:
+#     global_tokens = global_tokens[:10]
 
 
-vocab = [(tok['token'], tok.get('lang', 'arabic')) for tok in global_tokens]
-vocab = collect_info(vocab, wait_time=.1)
-deorphanize(wait_time=.1)
+# vocab = [(tok['token'], tok.get('lang', 'arabic')) for tok in global_tokens]
+# vocab = collect_info(vocab, wait_time=.1)
+# deorphanize(wait_time=.1)
 
-with open('tokens.json', 'w', encoding="utf8") as f:
-    f.write(json.dumps(vocab, indent=4, ensure_ascii=False))
+# with open('tokens.json', 'w', encoding="utf8") as f:
+#     f.write(json.dumps(vocab, indent=4, ensure_ascii=False))
+
+export_graph_to_html('graph.html')
--- scripts/visualize_interactive_graph.py
+++ scripts/visualize_interactive_graph.py
@@ -1,62 +1,54 @@
 from collections import Counter
-import dgl
 import sys
-from matplotlib import pyplot as plt
 import numpy as np
 import pymysql
 
-sys.path.append('.')
-from wiktionaryparser.utils import get_colormap
-from wiktionaryparser.preprocessing import Preprocessor
-from wiktionaryparser.graph import GraphBuilder
+from .utils import *
+from src.utils import get_colormap
 from pyvis.network import Network
-import networkx as nx
-
-conn = pymysql.connect(host="localhost", user="root", password="", db="knowledge_graph")
-builder = GraphBuilder(conn)
-graph = builder.build_graph("d2w", category_info=True, appendix_info=True, 
-    preprocessing_callback=Preprocessor(return_type="str"),
-)
 
 
+def export_graph_to_html(output_file='graph.html'):
+    graph = builder.build_graph("d2w", category_info=True, appendix_info=True, 
+        preprocessing_callback=Preprocessor(return_type="str"),
+    )
+    g = Network(directed=True)
+    n_color_palette = get_colormap(graph.ntypes, palette="tab10_r")
+    for n_i, ntype in enumerate(graph.ntypes):
+        node_ids = builder.node_ids[ntype]
+        ntype_voc = graph.nodes(ntype)
+        if len(ntype_voc) < 1:
+            continue
+        # print('{} ({}):'.format(ntype,len(ntype_voc)))
+        for i in ntype_voc:
+            i = i.item()
+            node_id = node_ids[i]
+            node = builder.vocab.get(node_id)
+            if node is not None:
+                node['i'] = i
+                # print('\t {}'.format(node['word']))
+                g.add_node(n_id=node['id'], label=node['word'], color=n_color_palette[ntype])
 
-g = Network(directed=True)
-n_color_palette = get_colormap(graph.ntypes, palette="tab10_r")
-for n_i, ntype in enumerate(graph.ntypes):
-    node_ids = builder.node_ids[ntype]
-    ntype_voc = graph.nodes(ntype)
-    if len(ntype_voc) < 1:
-        continue
-    # print('{} ({}):'.format(ntype,len(ntype_voc)))
-    for i in ntype_voc:
-        i = i.item()
-        node_id = node_ids[i]
-        node = builder.vocab.get(node_id)
-        if node is not None:
-            node['i'] = i
-            # print('\t {}'.format(node['word']))
-            g.add_node(n_id=node['id'], label=node['word'], color=n_color_palette[ntype])
+    # graph = dgl.to_homogeneous(builder.graph)
+    # print(graph.ndata)
+    # print(graph)
+    e_color_palette = get_colormap(set(graph.etypes), palette="Accent_r")
+    edgelist = []
+    for etype in graph.canonical_etypes:
+        stype, rel, dtype  = etype
+        src_node_idx = builder.node_ids[stype]
+        dst_node_idx = builder.node_ids[dtype]
+        edges = graph.edges(etype=etype)
+        edges = tuple(map(lambda x: x.tolist(), edges))
+        for s, d in zip(*edges):
+            s = src_node_idx[s]
+            d = dst_node_idx[d]
+            if None not in (s, d):
+                edgelist.append((s, rel, d))
 
-# graph = dgl.to_homogeneous(builder.graph)
-# print(graph.ndata)
-# print(graph)
-e_color_palette = get_colormap(set(graph.etypes), palette="Accent_r")
-edgelist = []
-for etype in graph.canonical_etypes:
-    stype, rel, dtype  = etype
-    src_node_idx = builder.node_ids[stype]
-    dst_node_idx = builder.node_ids[dtype]
-    edges = graph.edges(etype=etype)
-    edges = tuple(map(lambda x: x.tolist(), edges))
-    for s, d in zip(*edges):
-        s = src_node_idx[s]
-        d = dst_node_idx[d]
-        if None not in (s, d):
-            edgelist.append((s, rel, d))
+    edgelist = Counter(edgelist)
+    for (s, r, d), c in edgelist.items():
+        g.add_edge(source=s, to=d, label=r, value=np.log(c + 1), color=e_color_palette[r])
 
-edgelist = Counter(edgelist)
-for (s, r, d), c in edgelist.items():
-    g.add_edge(source=s, to=d, label=r, value=np.log(c + 1), color=e_color_palette[r])
-
-g.barnes_hut()
-g.save_graph('graph.html')
+    g.barnes_hut()
+    g.save_graph(output_file)
