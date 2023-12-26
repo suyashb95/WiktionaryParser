eab13142157d40efec99c78ba66e816d8ab9a95d
ZOUHEIRBN <zouheir.b97@gmail.com>
2023-12-26 01:40:29 +0100

    Added LLM based embedding using bert_based_multilingual

--- embed_text.py
+++ embed_text.py
@@ -3,5 +3,5 @@
 from src.feature_extraction import FeatureExtractor
 from scripts.utils import conn
 
-fe = FeatureExtractor(conn=conn)
+fe = FeatureExtractor(conn=conn, embedding_type="llm_embedding")
 print(fe('1596c68b103543e3c3bfee450d31588bf3448c5ed8dca4ae8b7b546747ada872'))
--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from sentence_transformers import SentenceTransformer
 
 
 class FeatureExtractor:
@@ -17,6 +18,8 @@ class FeatureExtractor:
         self.definitions_table = definitions_table
         self.edge_table = edge_table
 
+        self.sent_transformer = SentenceTransformer("bert-base-multilingual-cased")
+
     def __get_category_embedding(self, token_id):
         # Define the fields and join conditions for the query
         fields = "c.text"
@@ -26,15 +29,31 @@ class FeatureExtractor:
 
         # Execute the query using the read method of DatabaseClient
         result = self.conn.read(collection_name="categories c", fields=fields, joins=joins, conditions=conditions)
-        print(self.conn.query)
         # Processing the result into a dictionary
         categories = dict(Counter([row['text'] for row in result]))
 
         return categories
+    def __get_llm_embedding(self, token_id):
+        # Define the fields and join conditions for the query
+        fields = "def.text"
+        # fields = "*"
+        joins = [("words w", f"def.wordId = w.id")]
+        conditions = {"w.id": token_id}
+
+        # Execute the query using the read method of DatabaseClient
+        result = self.conn.read(collection_name="definitions def", fields=fields, joins=joins, conditions=conditions)
+        definitions = [row['text'] for row in result]
+        text_embeddings = self.sent_transformer.encode(definitions)
+        text_embeddings = text_embeddings.mean(axis=0)
+        return text_embeddings
     
+
     def __call__(self, token_id):
         if self.embedding_type == 'category_embedding':
             embeds = self.__get_category_embedding(token_id)
+            return embeds
+        elif self.embedding_type == 'llm_embedding':
+            embeds = self.__get_llm_embedding(token_id)
             return embeds
         else:
             return []
